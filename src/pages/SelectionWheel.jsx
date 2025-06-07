fb3e71115d380af0d927d604a80cdc590e29cd79
joyalcj4 <joyalchandrankunnel@example.com>
2025-06-07 16:03:26 +0530

    mobile changes

--- src/pages/SelectionWheel.jsx
+++ src/pages/SelectionWheel.jsx
@@ -24,7 +24,7 @@ const RadialMenu = ({ onSelect }) => {
     }, 300);
   };
 
-  const renderSegment = (position, label, segmentStyle,quote) => (
+  const renderSegment = (position, label, segmentStyle, quote) => (
     <div
       onMouseDown={() => handleHoldStart(position)}
       onMouseUp={handleHoldEnd}
@@ -34,31 +34,32 @@ const RadialMenu = ({ onSelect }) => {
       className={`radial-segment wheel-gradient group absolute ${segmentStyle}`}
     >
       <div className="flex flex-col items-center">
-        <span className="radial-label text-xs sm:text-lg md:text-xl sm:group-hover:text-2xl sm:group-hover:text-white">{label}</span>
-        <span className='opacity-0 text-xs sm:text-base text-white group-hover:opacity-100 '>{quote}</span>
+        <span className="radial-label sm:text-lg md:text-xl sm:group-hover:text-2xl sm:group-hover:text-white">
+          {label}
+        </span>
+        <span className="opacity-0 text-[10px] sm:text-base text-white group-hover:opacity-100">
+          {quote}
+        </span>
 
         <div className="radial-tooltip">
-          <p className="text-xs sm:text-sm font-light text-center">PRESS AND HOLD</p>
+          <p className="text-[10px] sm:text-sm font-light text-center">PRESS AND HOLD</p>
           <div className="radial-progress-bar">
-            {showTooltip ? (<div
-              className={`radial-progress-fill ${activeSegment === position ? 'w-full' : 'w-0'}`}
+            <div
+              className={`radial-progress-fill ${showTooltip && activeSegment === position ? 'w-full' : 'w-0'}`}
             ></div>
-            ) :
-              <div
-                className={`radial-progress-fill w-full`}
-              ></div>}
           </div>
         </div>
       </div>
+
     </div>
   );
 
   return (
     <div className="relative w-60 h-60 sm:w-102 sm:h-102 md:w-156 md:h-156 border rounded-full shadow-2xl flex items-center justify-center">
-      {renderSegment('projects', 'Projects', 'top-0 left-0 rounded-tl-full origin-bottom-right','See my works')}
-      {renderSegment('skills', 'Skills', 'top-0 right-0 rounded-tr-full origin-bottom-left','Always learning')}
-      {renderSegment('about', 'About', 'bottom-0 left-0 rounded-bl-full origin-top-right','The story')}
-      {renderSegment('contact', 'Contact', 'bottom-0 right-0 rounded-br-full origin-top-left','Keep in touch')}
+      {renderSegment('projects', 'Projects', 'top-0 left-0 rounded-tl-full origin-bottom-right', 'See my works')}
+      {renderSegment('skills', 'Skills', 'top-0 right-0 rounded-tr-full origin-bottom-left', 'Always learning')}
+      {renderSegment('about', 'About', 'bottom-0 left-0 rounded-bl-full origin-top-right', 'The story')}
+      {renderSegment('contact', 'Contact', 'bottom-0 right-0 rounded-br-full origin-top-left', 'Keep in touch')}
     </div>
   );
 };
